7bd93958c96c1c0f78dea067fb0c4828e26d9f22
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2023-11-13 10:24:54 +0200

    First commit!

---
+++ app.js
@@ -0,0 +1,65 @@
+const { dialog } = require("@electron/remote");
+const path = require("path");
+
+let paths = { lang: null, custom: null };
+
+function showNotification(title, body) {
+	new Notification({ title, body }).show();
+}
+
+const mergeFiles = () => {
+	console.log("Merge");
+};
+
+const updateElements = (type) => {
+	// Update file name and change button text
+	let element = document.getElementById(type + "Text");
+	element.innerText = paths[type];
+	element.scrollLeft = element.scrollWidth;
+	element.scrollLeft = element.scrollWidth;
+	document.getElementById(type + "Btn").innerText = "Change";
+
+	// Enable merge button if both files are selected
+	if (paths["lang"] && paths["custom"]) {
+		document.getElementById("mergeBtn").disabled = false;
+	}
+};
+
+// Show file select dialog to user
+const getLocation = async (type) => {
+	const defaultPath = path.join(
+		process.env["ProgramFiles(x86)"],
+		"Steam\\steamapps\\common\\Counter-Strike Global Offensive\\game\\csgo\\resource"
+	);
+
+	return dialog
+		.showOpenDialog({
+			defaultPath,
+			buttonLabel: `Select ${type} File`,
+			filters: [
+				{ name: "Text Files", extensions: ["txt"] },
+				{ name: "All Files", extensions: ["*"] },
+			],
+		})
+		.then((res) => res);
+};
+
+// Let user select a file and store the path to a variable
+const selectFile = async (type, text) => {
+	const location = await getLocation(text);
+	if (location.canceled) return;
+	paths[type] = location.filePaths[0];
+	updateElements(type);
+};
+
+document.getElementById("langBtn").addEventListener("click", () => {
+	selectFile("lang", "Language");
+});
+
+document.getElementById("customBtn").addEventListener("click", () => {
+	selectFile("custom", "Custom");
+});
+
+document.getElementById("mergeBtn").addEventListener("click", () => {
+	mergeFiles();
+});
---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+require("@electron/remote/main").initialize();
+
+const path = require("path");
+const url = require("url");
+// TODO: Remove electron reload stuff for release
+const electronReload = require("electron-reload");
+electronReload(__dirname, {
+	electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+	hardResetMethod: "exit",
+});
+
+let win;
+
+// Create app
+function createWindow() {
+	win = new BrowserWindow({
+		width: 800,
+		height: 700,
+		webPreferences: { enableRemoteModule: true, nodeIntegration: true, contextIsolation: false },
+	});
+
+	require("@electron/remote/main").enable(win.webContents);
+
+	win.loadURL(
+		url.format({
+			pathname: path.join(__dirname, "index.html"),
+			protocol: "file:",
+			slashes: true,
+		})
+	);
+
+	win.on("closed", function () {
+		win = null;
+	});
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+	if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+	if (win === null) createWindow();
+});
